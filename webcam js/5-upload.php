87a43704307caa3dafd1605a267c2fd3a37f3504
Hasan Damirli <72316307+hawanbeats@users.noreply.github.com>
2022-11-19 14:13:09 +0300

    Delete webcam js directory

--- webcam js/5-upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-// (A) SET THE DESTINATION FOLDER
-$source = $_FILES["upimage"]["tmp_name"];
-$destination = "uploaded.png";
-
-// (B) MOVE UPLOADED FILE TO DESTINATION
-echo move_uploaded_file($source, $destination) ? "OK" : "ERROR UPLOADING";
